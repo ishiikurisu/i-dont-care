9086173435ed353c57030840415b1aa9349cced9
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-12-04 20:47:41 -0200

    Laying ground for price options

--- js/app.js
+++ js/app.js
@@ -1,15 +1,5 @@
 function pickForThem() {
-    var options = [
-        'bomba',
-        'sushi',
-        'macarrão',
-        'o que você cozinhar',
-        'coreano',
-        'chinês',
-        'donut',
-        'self service',
-        'nada'
-    ];
+    var options = getOptions();
     var element = document.getElementById('choice');
     var index = Math.round((Math.random() * 100) % options.length);
     var selected = options[index];
@@ -18,5 +8,54 @@ function pickForThem() {
         selected = options[index];
     }
 
-    choice.innerHTML = selected;
+    element.innerHTML = selected.title;
+}
+
+function getOptions() {
+    return [
+        {
+            'title': 'macarrão',
+            'price': 30
+        },
+        {
+            'title': 'sushi',
+            'price': 40
+        },
+        {
+            'title': 'bomba',
+            'price': 20
+        },
+        {
+            'title': '\'burguer gourmet',
+            'price': 40
+        },
+        {
+            'title': 'australiano',
+            'price': 60
+        },
+        {
+            'title': 'coreano',
+            'price': 60
+        },
+        {
+            'title': 'self service',
+            'price': 20
+        },
+        {
+            'title': 'hot dog',
+            'price': 15
+        },
+        {
+            'title': 'chinês',
+            'price': 20
+        },
+        {
+            'title': 'o que você cozinhar',
+            'price': 10
+        },
+        {
+            'title': 'nada',
+            'price': 0
+        }
+    ]
 }
